94cac03c92fb969e07e55a623998119b7cf1294d
skdat6 <dobretheo@gmail.com>
2022-11-25 12:08:04 +0200

    added model for items, forms

--- shoppinglist/shopping_app/models.py
+++ shoppinglist/shopping_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=264)
+
+    class ItemCategories(models.TextChoices):
+        VEGETABLES = 'Vegetables'
+        FRUIT = 'Fruit'
+        MEAT = 'Meat'
+        DRINKS = 'Drinks'
+        SNACKS = 'Snacks'
+        DAIRY = 'Dairy'
+        PET = 'Pet'
+        SKINCARE = 'Skin Care'
+        HAIR = 'Hair Products'
+        AUTO = 'Auto-vehicle'
+        ELECTRONICS = 'Electronics'
+
+    category = models.CharField(choices=ItemCategories.choices, max_length=264)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.item_name
--- shoppinglist/shopping_app/views.py
+++ shoppinglist/shopping_app/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'shopping_app/index.html')
 
+def test(request):
+    return render(request, 'shopping_app/test.html')
